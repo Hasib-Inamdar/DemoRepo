db47aa1a9388bfde84f6a568ed5d050b92cc1834
Hasib-Inamdar <90450484+Hasib-Inamdar@users.noreply.github.com>
2025-07-02 15:21:47 +0530

    Update MGM.java

--- myJavaProg.java
+++ myJavaProg.java
@@ -2,7 +2,7 @@
 public class myJavaProg {
     public static void main(String[] args) {
         // Print Hello World
-        System.out.println("Hello, World!");
+        System.out.println("Hello, MGM!");
 
         
     }
