6032dd0304d3f73b06cbad4addb0ed26dbcd36b6
Vilen <vilen@gamil.com>
2025-07-03 10:50:42 +0530

    Add Substraction function

---
+++ CollabProg/Add.java
@@ -0,0 +1,9 @@
+class Add {
+    int num1 = 10;
+    int num2 = 20;
+    public int Addition(int num1, int num2) {
+       this.num1 =num1;
+       this.num2 = num2;
+       return this.num1 + this.num2;
+    }
+}
