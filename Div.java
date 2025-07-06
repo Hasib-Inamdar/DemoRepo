7e72d62573ba26e5bc420101d20adfa8ab2f8975
hasib_inamdar <hasib.inamdar.pro@gmail.com>
2025-07-07 01:01:21 +0530

    Fixed the syntax error

--- Div.java
+++ Div.java
@@ -1,8 +1,8 @@
 public class Div{
-    int a, int b;
+    int a, b;
     public float Division(int a, int b){
         this.a = a;
-        this.b = b
+        this.b = b;
 
         int result = a/b;
     }
